660c12ac1ed581a9879aa9f0fa1cfe7ea3c45fd1
92819 <928198962@qq.com>
2025-12-17 21:26:24 +0800

    完成归并排序

--- src/algthrom/排序/归并排序.java
+++ src/algthrom/排序/归并排序.java
@@ -2,5 +2,35 @@ package algthrom.排序;
 
 public class 归并排序 {
     //重写hashcode
+    //归并应该是里面最难的算法了
+    public static void main(String[] args) {
+        int[] arr = {5, 4, 3, 2, 1};
+        mergeSort(arr, 0, arr.length - 1);
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+    //什么是归并排序呢，就是将数组一直拆分，直到成为一个一个的数字，然后开始合并，边合并边排序，保证每一组内都是有序的
+    public static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+            merge(arr, left, mid, right);
+        }
+    }
 
+    private static void merge(int[] arr, int left, int mid, int right) {
+        int[] temp = new int[right - left + 1];
+        int i = left;
+        int j = mid + 1;
+        int k = 0;
+        while (i <= mid && j <= right) {
+            if (arr[i] <= arr[j]) {
+                temp[k++] = arr[i++];
+            } else {
+                temp[k++] = arr[j++];
+            }
+        }
+    }
 }
